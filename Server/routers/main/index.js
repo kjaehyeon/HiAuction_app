24fd7deede7d26c93edbb675aa1f5826f1b7234f
hyunjun <nhj2927@naver.com>
2021-12-05 16:09:21 +0900

    modify query

--- Server/routers/main/index.js
+++ Server/routers/main/index.js
@@ -58,7 +58,8 @@ module.exports = (pool) => {
                         + ' WHERE c_id = ?'
                         + ' AND Ad_id = (SELECT Ad_id FROM ADDRESS'
                                         + ' WHERE Name = ?)'
-                        + ' AND Expire_date > NOW()';
+                        + ' AND Expire_date > NOW()'
+                        + ` AND Is_end = '0'`;
             if (key) {
                 sql += ` AND LOWER(Name) LIKE '%${key.toLowerCase()}%'`
             }
@@ -130,7 +131,7 @@ module.exports = (pool) => {
     router.post('/bid', async (req, res) => {
         const {body: {user_id, price, item_id}} = req;
         let conn = null;
-
+        console.log(user_id, price, item_id);
         try {
             conn = await pool.getConnection(async conn => conn);
             const [result] = await conn.query('SELECT Min_bid_unit, Current_price,'
@@ -176,6 +177,7 @@ module.exports = (pool) => {
                 });
             }
         } catch (err) {
+            console.log(err)
             res.status(500).json({
                 message: err.message
             });
