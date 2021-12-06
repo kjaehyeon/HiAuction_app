243beb771def3320cbd5a28aafdaa31164b8d0cf
hyunjun <nhj2927@naver.com>
2021-12-07 01:26:28 +0900

    remove print

--- Server/routers/chat/index.js
+++ Server/routers/chat/index.js
@@ -140,7 +140,6 @@ module.exports = (pool) => {
             });
             res.status(200).json(chat_list);
         } catch (err) {
-            console.log(err);
             res.status(500).json({
                 message: err.message
             });
